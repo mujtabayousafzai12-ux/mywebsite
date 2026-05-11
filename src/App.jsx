a19bc0921bd272847961f00202affc92a1886a33
mujtabayousafzai12-ux <mujtabayousafzai12@gmail.com>
2026-05-12 01:50:24 +0500

    added profile image

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+<img
+  src="/pic.jpeg"
+  alt="profile"
+  className="w-40 h-40 rounded-full mx-auto border-4 border-green-500 mb-6 object-cover"
+/><section className="text-center mt-20">
 import { FaShieldAlt, FaTerminal, FaLock } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
